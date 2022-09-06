0854adc0dedbd8fc5fdba5687a7a6d25f37405ee
Seokjin Yoon <sjy5386@gmail.com>
2022-09-06 22:40:27 +0900

    Add config.yml

--- src/main/java/com/sysbot32/discoord/Main.java
+++ src/main/java/com/sysbot32/discoord/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         getCommand("discoord").setExecutor(new DiscoordCommandExecutor());
     }
 }
