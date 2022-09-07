88983fea16ebdc2a8b2ddd6c90a6be004a5edf14
Seokjin Yoon <sjy5386@gmail.com>
2022-09-07 20:26:54 +0900

    Update DiscoordCommandExecutor

--- src/main/java/com/sysbot32/discoord/commands/DiscoordCommandExecutor.java
+++ src/main/java/com/sysbot32/discoord/commands/DiscoordCommandExecutor.java
@@ -8,26 +8,32 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
+
 public class DiscoordCommandExecutor implements CommandExecutor {
     @Override
-    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+    public boolean onCommand(@Nonnull final CommandSender sender, @Nonnull final Command command, @Nonnull final String label, @Nonnull final String[] args) {
         if (args.length < 2) {
             return false;
         }
 
         if ("add".equalsIgnoreCase(args[0])) {
-            if (sender instanceof final Player player) {
-                final Location location = player.getLocation();
-                final String message = args[1] + ": " + location.getX() + " / " + location.getY() + " / " + location.getZ();
-                sender.sendMessage(message);
-                new DiscordWebhook().execute(DiscordWebhookRequest.builder()
-                        .content(message)
-                        .username(player.getName())
-                        .build());
-            } else {
-                return false;
-            }
+            return this.add(sender, command, label, args);
         }
-        return true;
+        return false;
+    }
+
+    private boolean add(@Nonnull final CommandSender sender, @Nonnull final Command command, @Nonnull final String label, @Nonnull final String[] args) {
+        if (sender instanceof final Player player) {
+            final Location location = player.getLocation();
+            final String message = args[1] + ": " + location.getX() + " / " + location.getY() + " / " + location.getZ();
+            sender.sendMessage(message);
+            new DiscordWebhook().execute(DiscordWebhookRequest.builder()
+                    .content(message)
+                    .username(player.getName())
+                    .build());
+            return true;
+        }
+        return false;
     }
 }
